b99f2b593344b99b0116a31c6e3515ca6ca9c2be
woodruffr4 <woodruffr4@gmail.com>
2019-03-24 17:06:15 -0400

    Update layout

--- like_button.js
+++ like_button.js
@@ -1,25 +1,46 @@
-'use strict';
-
-const e = React.createElement;
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { liked: false, counter: 0 };
+    this.setLiked = this.setLiked.bind(this);
+    this.incrementCounter = this.incrementCounter.bind(this);
+  }
+
+  setLiked() {
+    this.setState({ liked: true });
+  }
+
+  incrementCounter() {
+    var count = this.state.counter;
+    count = count + 1;
+    this.setState({ counter: count });
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+    if(this.state.counter === 0) {
+      return (
+        <button onClick={this.incrementCounter}> Like </button>
+  
+      );
+    } else {
+      return (
+
+        <div>
+  
+          <button onClick={this.incrementCounter}> Like </button>
+  
+          <p> You liked this ({this.state.counter}) </p>
+  
+        </div>
+  
+      );
+    }
+    
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(<LikeButton />, document.getElementById('like_button_container'));
